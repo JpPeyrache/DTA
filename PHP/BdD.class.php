a1cef1dd4c0a02b8a87b1284968264394b5590c3
JpPeyrache <peyrachejph@hotmail.com>
2017-02-15 17:05:38 +0100

    Ajout classe BdD

---
+++ PHP/BdD.class.php
@@ -0,0 +1,31 @@
+<?php
+class BdD{
+
+  private $_host = "localhost";
+  private $_user = "root";
+  private $_pass = "root";
+  private $_base = "";
+
+  private $_connexion;
+
+  public function __construct(){
+    try{
+      $path = 'mysql:host='.$this->_host.';dbname='.$this->_base.';charset=utf8';
+      $this->_connexion = new PDO($path, $this->_user, $this->_pass, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    }catch (Exception $e){
+      die('Erreur : ' . $e->getMessage());
+    }
+  }
+
+  public function prepReq($query,$array){
+    $req = $this->_connexion->prepare($query);
+    $req->execute($array);
+    return $req;
+  }
+
+  public function req($query){
+    return $this->_connexion->query($query);
+  }
+
+}
+?>
