270765a387c6f3b94a549b3a487fb76806dbeb3d
JpPeyrache <peyrachejph@hotmail.com>
2017-03-30 14:36:38 +0200

    Ajout Marvel

---
+++ Ajax/marvel.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta charset="utf-8">
+    <title>Liste des comics Marvel</title>
+
+    <script src="hmac-md5.js"></script>
+    <script src="../JQuery/jquery-3.1.1.js"></script>
+
+    <style>
+      .comic{
+        background-color: antiquewhite;
+        margin: 20px;
+      }
+      .description{
+        background-color: lightgrey;
+        padding: 20px;
+      }
+    </style>
+  </head>
+
+  <body>
+
+    <div id="liste">
+    </div>
+
+    <script>
+      $(document).ready(function(){
+        // Instanciation des différents éléments
+        var date = new Date();
+        var ts = date.getTime().toString();
+        var publickey = "b780d336666ceb52c1538d44811b3e5c";
+        var privatekey = "03d1d0d0ec2e0a5d039c22c16ca1a829aff99400";
+        var hash = CryptoJS.MD5(ts+privatekey+publickey);
+
+        // Construction de l'URL
+        var url = "https://gateway.marvel.com:443/v1/public/comics?ts="+ts+"&apikey="+publickey+"&hash="+hash;
+
+        // Récupération du JSON à l'URL indiquée
+        $.getJSON(url,function(data) {
+          // Récupération de la liste des comics
+          var liste = data["data"]["results"];
+          // On boucle sur tous les comics
+          for(var comic in liste){
+            // On construit la div à rajouter pour chaque élément
+            var elem = liste[comic];
+            var newDiv =   $("<div class=\"comic\" id="+elem["id"]+">"+elem["title"]+" </div>");
+            var newLink = $("<a href=\"#\">Description</a>");
+
+            // Construction du comportement en cas de clic
+            $(newLink).click(function(){
+              // Récupération de l'id du comic
+              var id = $(this).parent().attr("id");
+
+              // Construction de l'URL
+              var url = "https://gateway.marvel.com:443/v1/public/comics/"+id+"?ts="+ts+"&apikey="+publickey+"&hash="+hash;
+
+              // Récupération du JSON à l'URL indiquée
+              $.getJSON(url,function(data) {
+                // On récupère la description du comic
+                var results = data["data"]["results"];
+                var description = results[0]["description"];
+                var newDiv =   $("<div class=\"description\">"+description+" </div>");
+                $("#"+id).append(newDiv);
+              });
+            });
+            $(newDiv).append(newLink);
+            $(newDiv).appendTo("#liste");
+          }
+        });
+      });
+    </script>
+
+  </body>
+
+</html>
